df22099552c6005a49d48d99f430c9fcb4cbd0af
Dooshine <doyungi0304@gmail.com>
2023-09-13 20:07:31 +0900

    230912

--- src/main/java/com/kh/toyproject/rest/ChatGptRestController.java
+++ src/main/java/com/kh/toyproject/rest/ChatGptRestController.java
@@ -1,23 +1,22 @@
 package com.kh.toyproject.rest;
 
-import java.net.URISyntaxException;
-
+import com.kh.toyproject.service.ChatGptService;
+import com.kh.toyproject.vo.WeatherResponseVO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.kh.toyproject.service.ChatGptService;
-import com.kh.toyproject.vo.WeatherResponseVO;
+import java.net.URISyntaxException;
 
 @RestController
 @RequestMapping("/chat")
 public class ChatGptRestController {
-	
+
 	@Autowired
 	private ChatGptService gptService;
-	
+
 	@PostMapping("/")
 	public String getGpt(@RequestBody WeatherResponseVO vo) throws URISyntaxException {
 		return gptService.chatRequest(vo);
